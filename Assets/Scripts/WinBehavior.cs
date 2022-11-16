34e4243f1d9653dfd4c99f9ce1a5fdf59b090555
Gr-monaco <gr-monaco@hotmail.com>
2022-11-15 23:34:29 -0300

    Adicionar objetivo final

---
+++ Assets/Scripts/WinBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WinBehavior : MonoBehaviour
+{
+
+  private GameManager gameManagerComponent;
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    gameManagerComponent = FindObjectOfType<GameManager>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.transform.parent.name == "Player" && GetComponent<MeshRenderer>().enabled)
+    {
+        Debug.Log(gameManagerComponent.pointsTextObject.GetComponent<Text>().text);
+        Debug.Log(gameManagerComponent.timeRemaining);
+      gameManagerComponent.endGameWithText("O jogador venceu! Teve " + (int.Parse(gameManagerComponent.pointsTextObject.GetComponent<Text>().text) * gameManagerComponent.timeRemaining).ToString() + "pontos " );
+      GetComponent<MeshRenderer>().enabled = false;
+    }
+  }
+
+  public void reabilitar()
+  {
+    GetComponent<MeshRenderer>().enabled = true;
+
+  }
+}
